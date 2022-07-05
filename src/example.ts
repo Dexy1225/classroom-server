4b251827e40983c71a4e8ba47a9a99183deabea8
Kristine <keran.xin@gmail.com>
2022-07-05 16:54:28 -0400

    Updated version with all routes work

--- src/example.ts
+++ src/example.ts
@@ -8,52 +8,52 @@ app.use(express.json())
 let students = [
     {
         'Name': 'Nick',
-        'Id': '1',
-        'Assignment1': '80',
-        'Assignment2': '90',
-        'Assignment3': '100'
+        'Id': 1,
+        'Assignment1': 80,
+        'Assignment2': 90,
+        'Assignment3': 100
     },
     {
         'Name': 'Winston',
-        'Id': '2',
-        'Assignment1': '94',
-        'Assignment2': '77',
-        'Assignment3': '100'
+        'Id': 2,
+        'Assignment1': 94,
+        'Assignment2': 77,
+        'Assignment3': 100
     },
     {
         'Name': 'Calvin',
-        'Id': '3',
-        'Assignment1': '66',
-        'Assignment2': '77',
-        'Assignment3': '88'
+        'Id': 3,
+        'Assignment1': 66,
+        'Assignment2': 77,
+        'Assignment3': 88
     },
     {
         'Name': 'Julian',
-        'Id': '4',
-        'Assignment1': '68',
-        'Assignment2': '77',
-        'Assignment3': '68'
+        'Id': 4,
+        'Assignment1': 68,
+        'Assignment2': 77,
+        'Assignment3': 68
     },
     {
         'Name': 'Timur',
-        'Id': '5',
-        'Assignment1': '100',
-        'Assignment2': '92',
-        'Assignment3': '94'
+        'Id': 5,
+        'Assignment1': 100,
+        'Assignment2': 92,
+        'Assignment3': 94
     },
     {
         'Name': 'Katherine',
-        'Id': '6',
-        'Assignment1': '90',
-        'Assignment2': '88',
-        'Assignment3': '89'
+        'Id': 6,
+        'Assignment1': 90,
+        'Assignment2': 88,
+        'Assignment3': 89
     },
     {
         'Name': 'Kristine',
-        'Id': '7',
-        'Assignment1': '77',
-        'Assignment2': '77',
-        'Assignment3': '77'
+        'Id': 7,
+        'Assignment1': 77,
+        'Assignment2': 77,
+        'Assignment3': 77
     }
 ];
 
@@ -84,12 +84,13 @@ app.get('/students/:id/:assign_num', (req: Request, res:Response) =>{
 
 app.post('/students/:id/:assign_num', (req: Request, res:Response) =>{
     const isValid = false;
-    let new_grade = res.json(req.body.grade); //pasing {"grade": 66} as body
-    //let id = parseInt(req.params.id) - 1;
-    //let student: any = students[id];
-    //let a: string= 'Assignment' + req.params.assign_num;
-    //student[a] = new_grade;
-    res.json(new_grade);
+    let new_grade = req.body['grade']; //passing {"grade": 66} as body
+    let id = parseInt(req.params.id) - 1;
+    let student: any = students[id];
+    let a: string= 'Assignment' + req.params.assign_num;
+    student[a] = new_grade;
+    res.redirect(`/students/${req.params.id}`);
+    
 })
 
 
